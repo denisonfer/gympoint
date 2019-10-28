0e0650c8327449207767cff1a12aa7453f625b01
Filubil <denison.shenry@gmail.com>
2019-10-28 10:31:49 -0300

    Criado a tabela de alunos e controller.js para as rotas de aluno

---
+++ src/database/migrations/20191028114949-create-students.js
@@ -0,0 +1,41 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('students', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      age: {
+        type: Sequelize.INTEGER,
+      },
+      weight: {
+        type: Sequelize.FLOAT,
+      },
+      height: {
+        type: Sequelize.FLOAT,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: queryInterface => {
+    return queryInterface.dropTable('students');
+  },
+};
