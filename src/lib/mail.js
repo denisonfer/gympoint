9dc5db0a77795799426b716c8b8c4743bfff1bc3
Filubil <denison.shenry@gmail.com>
2019-10-28 14:43:20 -0300

    Criado a configuração e o modelo de email

---
+++ src/lib/mail.js
@@ -0,0 +1,45 @@
+import nodemailer from 'nodemailer';
+import { resolve } from 'path';
+import exphbs from 'express-handlebars';
+import nodemailerhbs from 'nodemailer-express-handlebars';
+import emailConfig from '../config/nodemailer';
+
+class Mail {
+  constructor() {
+    const { host, port, secure, auth } = emailConfig;
+    this.transporter = nodemailer.createTransport({
+      host,
+      port,
+      secure,
+      auth: auth.user ? auth : null,
+    });
+
+    this.configTemplate();
+  }
+
+  configTemplate() {
+    const viewPath = resolve(__dirname, '..', 'app', 'views', 'emails');
+    this.transporter.use(
+      'compile',
+      nodemailerhbs({
+        viewEngine: exphbs.create({
+          layoutsDir: resolve(viewPath, 'layouts'),
+          partialsDir: resolve(viewPath, 'partials'),
+          defaultLayout: 'default',
+          extname: '.hbs',
+        }),
+        viewPath,
+        extName: '.hbs',
+      })
+    );
+  }
+
+  sendMail(message) {
+    return this.transporter.sendMail({
+      ...emailConfig.default,
+      ...message,
+    });
+  }
+}
+
+export default new Mail();
